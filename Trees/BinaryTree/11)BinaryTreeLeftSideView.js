f738380cb3527200da75db15fe2a27f2d95133f2
unknown <kamatabhishek14@gmail.com>
2023-04-05 01:02:47 +0530

    About Trees and solved LEET CODE problems on Trees

---
+++ Trees/BinaryTree/11)BinaryTreeLeftSideView.js
@@ -0,0 +1,93 @@
+//Here also the same initially we will create a array of all levels and then put the 1st element of each array into the output
+
+
+function createNode(value) {
+    return {
+      data: value,
+      next: null,
+    };
+  }
+  
+  class CustomQueue {
+    constructor() {
+      this.head = null; // front
+      this.tail = null; // back
+    }
+    enqueue(x) {
+      let newNode = createNode(x);
+      //add at tail
+      if (this.tail == null) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+    }
+  
+    dequeue() {
+      if (this.head == null) return;
+      let nextNode = this.head.next;
+      this.head.next = null;
+      this.head = nextNode;
+      if (this.head == null) {
+        this.tail = null;
+      }
+    }
+  
+    getFront() {
+      if (this.head == null) return;
+      return this.head.data;
+    }
+  
+    getback() {
+      if (this.tail == null) return;
+      return this.tail.data;
+    }
+  
+    empty() {
+      return this.head == null;
+    }
+  }
+
+  function RightSideView(root) {
+
+    if(root == null){
+             return []
+         }
+       let qu = new CustomQueue()
+       qu.enqueue(root);
+       qu.enqueue(null);
+       let result = [];
+       let temp = [];
+     
+     while(true){
+         let front = qu.getFront()
+         qu.dequeue()
+         if(front == null){
+       
+         if(qu.empty()){
+         break;
+          }
+     
+         qu.enqueue(null)
+         result.push(temp)
+         temp = []
+         continue
+         }
+         temp.push(front.val);
+     
+         if(front.left){
+             qu.enqueue(front.left)
+         }
+         if(front.right){
+             qu.enqueue(front.right)
+         }
+     }result.push(temp)
+   
+     let result1 = [];
+     for(let i = 0 ; i < result.length ;i++){
+         result1.push(result[i][0])
+     }  
+     return result1
+   };
